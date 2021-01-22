bff2bf04db2bb79e22ffc5bc468d6cfe7e9ae4f4
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-01-22 21:03:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,4 +6,4 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["commands", "commands@xploaderzxbot"]))
 async def command(client, message):
-    await message.reply_text("/upload - Uploads the URL to Telegram as File \n/me - Shows your plan \n/upgrade - Upgrades your existing plan \n/generatecustomthumbnail - Saves the photo as Thumbnail for ur all Files/Videos \n/deletethumbnail - Delete the saved thumbnail \n/help - Shows help Message \n/rename - Renames any file \n/generatescss - Generates screenshots from Video(can be file also) \n/unzip - Unzip the uploads files")
+    await message.reply_text("Here r the Commands \n/upload - Uploads the URL to Telegram as File \n/me - Shows your plan \n/upgrade - Upgrades your existing plan \n/generatecustomthumbnail - Saves the photo as Thumbnail for ur all Files/Videos \n/deletethumbnail - Delete the saved thumbnail \n/help - Shows help Message \n/rename - Renames any file \n/generatescss - Generates screenshots from Video(can be file also) \n/unzip - Unzip and the uploads files")

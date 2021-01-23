f2afdf58b0f5108dee6560934df19aa519ccd0ba
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-01-23 08:14:59 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -45,7 +45,21 @@ async def button(bot, update):
             message_ids=update.message.message_id,
             revoke=True
         )
-        return
+     if update_channel:
+        try:
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked":
+               await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
+            await update.reply_text(
+                text="**Join My Updates Channel to use ME 😎 🤭**",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+              ])
+            )
+        return False
     # logger.info(update)
     cb_data = update.data
     if ":" in cb_data:

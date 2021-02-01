24ca42c3eeb7e6fee90a6ec3af55412f8b748c65
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-02-01 21:35:59 +0530

    Update c_2_audio.py

--- plugins/c_2_audio.py
+++ plugins/c_2_audio.py
@@ -11,3 +11,112 @@ from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from telethon.tl.functions.messages import SendMediaRequest
 
 from . import make_gif, progress
+
+
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await edit_or_reply(event, "```Reply to any media file.```")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.media:
+        await edit_or_reply(event, "reply to media file")
+        return
+    input_str = event.pattern_match.group(1)
+    if input_str is None:
+        await edit_or_reply(event, "try `.nfc voice` or`.nfc mp3`")
+        return
+    if input_str in ["mp3", "voice"]:
+        event = await edit_or_reply(event, "converting...")
+    else:
+        await edit_or_reply(event, "try `.nfc voice` or`.nfc mp3`")
+        return
+    try:
+        start = datetime.now()
+        c_time = time.time()
+        downloaded_file_name = await event.client.download_media(
+            reply_message,
+            Config.TMP_DOWNLOAD_DIRECTORY,
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(d, t, event, c_time, "trying to download")
+            ),
+        )
+    except Exception as e:
+        await event.edit(str(e))
+    else:
+        end = datetime.now()
+        ms = (end - start).seconds
+        await event.edit(
+            "Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms)
+        )
+        new_required_file_name = ""
+        new_required_file_caption = ""
+        command_to_run = []
+        voice_note = False
+        supports_streaming = False
+        if input_str == "voice":
+            new_required_file_caption = "voice_" + str(round(time.time())) + ".opus"
+            new_required_file_name = (
+                Config.TMP_DOWNLOAD_DIRECTORY + "/" + new_required_file_caption
+            )
+            command_to_run = [
+                "ffmpeg",
+                "-i",
+                downloaded_file_name,
+                "-map",
+                "0:a",
+                "-codec:a",
+                "libopus",
+                "-b:a",
+                "100k",
+                "-vbr",
+                "on",
+                new_required_file_name,
+            ]
+            voice_note = True
+            supports_streaming = True
+        elif input_str == "mp3":
+            new_required_file_caption = "mp3_" + str(round(time.time())) + ".mp3"
+            new_required_file_name = (
+                Config.TMP_DOWNLOAD_DIRECTORY + "/" + new_required_file_caption
+            )
+            command_to_run = [
+                "ffmpeg",
+                "-i",
+                downloaded_file_name,
+                "-vn",
+                new_required_file_name,
+            ]
+            voice_note = False
+            supports_streaming = True
+        else:
+            await event.edit("not supported")
+            os.remove(downloaded_file_name)
+            return
+        logger.info(command_to_run)
+        process = await asyncio.create_subprocess_exec(
+            *command_to_run,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await process.communicate()
+        stderr.decode().strip()
+        stdout.decode().strip()
+        os.remove(downloaded_file_name)
+        if os.path.exists(new_required_file_name):
+            force_document = False
+            await event.client.send_file(
+                entity=event.chat_id,
+                file=new_required_file_name,
+                allow_cache=False,
+                silent=True,
+                force_document=force_document,
+                voice_note=voice_note,
+                supports_streaming=supports_streaming,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, event, c_time, "trying to upload")
+                ),
+            )
+            os.remove(new_required_file_name)
+            await event.delete()

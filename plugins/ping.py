625693db627d8cdf97982564bfeb48256286ae4c
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-03-23 14:23:36 +0530

    New Ping

--- plugins/ping.py
+++ plugins/ping.py
@@ -17,7 +17,3 @@ async def ping(bot, message):
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
-    
-@Bot.on_message(filters.command("bc"))
-async def help(bot, cmd):
-    await bot.send_message(chat_id=Config.LOG_CHANNEL, text=f"#S")

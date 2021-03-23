cd553c05d18a7680197c46bcabb3c03b8671348c
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-03-23 14:59:30 +0530

    Update logs.py

--- plugins/logs.py
+++ plugins/logs.py
@@ -1,6 +1,5 @@
 import os
 import time
-import pyrogram
 from pyrogram import Client, filters, errors
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, PeerIdInvalid
@@ -12,6 +11,6 @@ else:
     
 Bot = Client(Config.SESSION_NAME, bot_token=Config.TG_BOT_TOKEN, api_id=Config.APP_ID, api_hash=Config.API_HASH) 
 
-@Client.on_message(pyrogram.filters.command(["bc"]))
+@Bot.on_message(pyrogram.filters.command(["bc"]))
 async def help(bot, cmd):
     await bot.send_message(chat_id=Config.LOG_CHANNEL, text=f"#S")

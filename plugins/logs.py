f28742216591ac5aaff7860d1b849ae0593cb187
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-03-23 15:01:39 +0530

    Update logs.py

--- plugins/logs.py
+++ plugins/logs.py
@@ -11,6 +11,6 @@ else:
     
 Bot = Client(Config.SESSION_NAME, bot_token=Config.TG_BOT_TOKEN, api_id=Config.APP_ID, api_hash=Config.API_HASH) 
 
-@Bot.on_message(pyrogram.filters.command(["bc"]))
+@Bot.on_message(filters.command(["bc"]))
 async def help(bot, cmd):
     await bot.send_message(chat_id=Config.LOG_CHANNEL, text=f"#S")

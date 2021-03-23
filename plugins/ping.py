9b8751e65de0aa103a9ee2281cdb4ad6c44389c8
Vinayak703 <76476634+vinayakext2@users.noreply.github.com>
2021-03-23 14:06:46 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -7,6 +7,8 @@ if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
+    
+Bot = Client(Config.SESSION_NAME, bot_token=Config.BOT_TOKEN, api_id=Config.API_ID, api_hash=Config.API_HASH)    
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["ping", "ping@xploaderzxbot"]))
 async def ping(bot, message):
@@ -16,4 +18,6 @@ async def ping(bot, message):
     time_taken_s = (end_t - start_t) * 1000
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
     
+@Bot.on_message(filters.command("bc"))
+async def help(bot, cmd):
     await bot.send_message(chat_id=Config.LOG_CHANNEL, text=f"#S")
